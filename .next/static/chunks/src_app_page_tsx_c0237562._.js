1512ef620fc28a20835155df538e366be9b38d94
yoyooyooo <315915093@qq.com>
2025-05-30 21:07:57 +0800

    Add .next/static/chunks/src_app_page_tsx_c0237562._.js 1748610476741

---
+++ .next/static/chunks/src_app_page_tsx_c0237562._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_44559b09._.js"
+],
+    source: "dynamic"
+});
