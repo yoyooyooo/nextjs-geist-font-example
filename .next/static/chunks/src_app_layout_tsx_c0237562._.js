aeca670ab204b623bb955058d1541c741cb6a19f
yoyooyooo <315915093@qq.com>
2025-05-30 21:08:22 +0800

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1748610501865

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_dist_05476590._.js",
+  "static/chunks/src_app_layout_tsx_bbf21e81._.js",
+  "static/chunks/src_app_globals_b805903d.css"
+],
+    source: "dynamic"
+});
